3300712f2e1432a29cdda268ce87d8ae1ae606a8
Kartoffelkopp <mr.kitopa@gmail.com>
2022-07-04 20:45:45 +0200

    minor changes to columns_to_keep in json

--- capstone_json_functions.py
+++ capstone_json_functions.py
@@ -3,10 +3,10 @@ import json
 import os
 
 keys_to_keep = ["name", "goal", "pledged", "state", "slug",
-                "country", "country_displayable_name", "currency",
+                "country", "currency", 'state_changed_at',
                 "deadline", "created_at", "launched_at",
-                "backers_count", "usd_pledged","current_currency",
-                "creator","category"]
+                "backers_count", "usd_pledged", "current_currency",
+                "creator"]
 
 all_keys = ['id', 'photo', 'name', 'blurb', 'goal', 'pledged',
             'state', 'slug', 'disable_communication', 'country',
@@ -21,10 +21,9 @@ all_keys = ['id', 'photo', 'name', 'blurb', 'goal', 'pledged',
 keys_to_dump = [x for x in all_keys if x not in keys_to_keep]
 
 columns_to_keep = ['name', 'goal', 'pledged', 'state', 'slug', 'country',
-       'country_displayable_name', 'currency', 'deadline', 'created_at',
-       'launched_at', 'backers_count', 'usd_pledged', 'current_currency',
-       'creator_id', 'creator_name', 'creator_slug','category_id',
-       'category_name', 'category_analytics_name', 'category_slug']
+                   'currency', 'deadline', 'created_at',
+                   'launched_at', 'backers_count', 'usd_pledged', 
+                   'creator_id', 'creator_name', 'state_changed_at']
 
 
 def list_of_lines(filepath):
@@ -32,7 +31,8 @@ def list_of_lines(filepath):
         lines = fp.readlines()
     return lines
 
-def build_df_from_lines_list(line_list,category=34):
+
+def build_df_from_lines_list(line_list, category=34):
     df = pd.DataFrame()
     for line in line_list:
         try:
@@ -43,41 +43,43 @@ def build_df_from_lines_list(line_list,category=34):
                         line_dict.pop(key)
                     except:
                         continue
-                df_temp = pd.json_normalize(line_dict,sep='_')
-                df = pd.concat([df,df_temp])
+                df_temp = pd.json_normalize(line_dict, sep='_')
+                df = pd.concat([df, df_temp])
         except:
             continue
     return df
 
 
-def list_of_categories(filepath,dumpfile=False):
-    #read file
+def list_of_categories(filepath, dumpfile=False):
+    # read file
     with open(filepath) as fp:
         lines = fp.readlines()
     # build dict with all the category ids and "slug" names
     category_dict = dict()
     for line in lines:
         line_dict = json.loads(line[line.index('{"id":'):-2])
-        category_dict[line_dict["category"]["id"]] = line_dict["category"]["slug"]
+        category_dict[line_dict["category"]["id"]
+                      ] = line_dict["category"]["slug"]
     # build list from dict and sort it
     list_of_categories = []
     for key, value in category_dict.items():
-        list_of_categories.append([key,value])
+        list_of_categories.append([key, value])
     list_of_categories.sort()
     # dump result into file
     if dumpfile:
-        with open("kickstarter_categories.txt","w") as fp:
+        with open("kickstarter_categories.txt", "w") as fp:
             for item in list_of_categories:
                 fp.writelines(f"id: {item[0]} ---> {item[1]}\n")
     return list_of_categories
 
+
 def rename_kickstarter_files(directory):
     for filename in os.listdir(directory):
         # Construct old file name
-        source =  f"{directory}{filename}"
+        source = f"{directory}{filename}"
         # Adding the count to the new file name and extension
         if filename.startswith("Kickstarter"):
             destination = f"{directory}{filename[12: 22]}.json"
-            #Renaming the file
+            # Renaming the file
             os.rename(source, destination)
             print(f"Renamed: {source} ---> {destination}")
